b01c8518533471307940a379ac10cd643631a13d
Gabriel M <79814036+yi-sht@users.noreply.github.com>
2022-02-14 15:20:06 -0300

    Create Stop_gninnipS_My_sdroW!.rb

---
+++ ruby/Stop_gninnipS_My_sdroW!.rb
@@ -0,0 +1,17 @@
+#Write a function that takes in a string of one or more words, and returns the same string, but with all five or 
+#more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and 
+#spaces. Spaces will be included only when more than one word is present.
+
+#Examples: 
+#spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+#spinWords( "This is a test") => returns "This is a test" 
+#spinWords( "This is another test" )=> returns "This is rehtona test"
+
+#answer:
+
+def spin_words(string)
+  #TODO
+  new_string = ""
+  string.split(" ").each {|word| word.length > 4 ? new_string+=  word.reverse!+ " " : new_string+= word + " " }
+  new_string[0..-2]
+end
